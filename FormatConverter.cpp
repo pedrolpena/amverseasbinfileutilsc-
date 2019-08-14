76c6f9dc8df85b77a429c3b9fb1e4c88b975c2f2
qbancoffee <pedro.pena@noaa.gov>
2019-08-14 07:40:26 -0400

    began adding NDC format

--- FormatConverter.cpp
+++ FormatConverter.cpp
@@ -95,7 +95,7 @@ std::string FormatConverter::getASCII() {
     std::vector<std::vector<double>> depthsAndTemps;  //array of doubles that holds depths & temperature measurements
     DepthCalculator *dc = new DepthCalculator(*xBTprofile);
     depthsAndTemps = dc->getDepthsAndTemperaturePoints();
-    char buf[120];
+    //char buf[120];
 
     tmp += "Call Sign                      | " + xBTprofile->getWMOId() + "\n";
     tmp += "Latitude                       | " + decimalDegreesLatToDMSAOML(xBTprofile->getLatitude()) + "\n";
@@ -196,7 +196,7 @@ std::string FormatConverter::getASCIIEDF(std::string fileName, double salinity) 
 	std::vector<std::vector<double>> depthsAndTemps;               //array of doubles that holds depths & temperature measurements
     DepthCalculator *dc = new DepthCalculator(*xBTprofile);
     depthsAndTemps = dc->getDepthsAndTemperaturePoints();
-    char buf[120];
+    //char buf[120];
     std::string tmp = "";
     fileName = fileName.substr(fileName.find_last_of("/\\") + 1); // extract filename from path
 
@@ -271,5 +271,76 @@ std::string FormatConverter::getASCIIEDF(std::string fileName, double salinity) 
     } //end for
     return tmp;
 
+
+}
+
+std::string FormatConverter::getASCIINDC() {
+
+	std::string tmp = "";
+
+
+
+	tmp += " SEAS Version:  "  + std::to_string(xBTprofile->getSeasVersion()) + "\n";
+	tmp += " Ship Name: "      + xBTprofile->getShipName() + "\n";
+	tmp += " Call Sign: "      + xBTprofile->getWMOId() + "\n";
+	tmp += " Lloyds Number:  " + std::to_string(xBTprofile->getLloyds()) + "\n";
+	tmp += " Date/Time(dd/mm/yyyy): ";
+	tmp += " Latitude(ddd.ddd): " + decimalDegreesLonToDMSAOML(xBTprofile->getLatitude()) + "\n";
+	tmp += " Longitude(ddd.ddd): " + decimalDegreesLonToDMSAOML(xBTprofile->getLongitude()) + "\n";
+	tmp += " Probe Type: " + XBTProbe::getProbeDescription(xBTprofile->getInstrumentType()) + "\n";
+	tmp += " Probe Code:  " + std::to_string(xBTprofile->getInstrumentType()) + "\n";
+	tmp += " Probe Serial No: " + std::to_string(xBTprofile->getProbeSerialNumber()) + "\n";
+	tmp += " Recorder Type: " + XBTRecorder::getRecorderDescription(xBTprofile->getRecorderType()) + "\n";
+	tmp += " Recorder Code:  " + std::to_string(xBTprofile->getRecorderType()) + "\n";
+	sprintf(buf,"%05.2f", xBTprofile->getDryBulbTemperature() - 273.15);
+	tmp += " Dry Bulb Temp:  " + std::string(buf) + "\n";
+	tmp += " Wind Instr Type: " + std::to_string(xBTprofile->getWindInstrumentType()) + "\n";
+	sprintf(buf,"%04.1f",  xBTprofile->getWindSpeed());
+	tmp += " Wind Speed: " + std::string(buf) + "\n";
+	sprintf(buf,"%04.1f",  xBTprofile->getWindDirection());
+	tmp += " Wind Dir:   " + std::string(buf) + "\n";
+	tmp += " Current Measurement Method: " + std::to_string(xBTprofile->getSeaSurfaceCurrentMeasurementMethod()) + "\n";
+	sprintf(buf,"%2.2f",  xBTprofile->getSeaSurfaceCurrentSpeed());
+	tmp += " Current Speed: " + std::string(buf) + "\n";
+	tmp += " Current Dir:   " + std::to_string(xBTprofile->getSeaSurfaceCurrentDirection()) + "\n";
+	tmp += " Bottom Depth:    " + std::to_string(xBTprofile->getTotalWaterDepth()) + "\n";
+
+    sprintf(buf,"%x", xBTprofile->getUniqueTag());
+    std::string s = std::string(buf);
+
+    for(unsigned int i =0; i < s.size() ; i++) {
+		s[i]=std::toupper(s[i]);
+	}
+	tmp += " SEAS ID: " + s + "\n";
+	sprintf(buf,"%d", (int) xBTprofile->getShipSpeed());
+	tmp += " Ship Speed at Launch (knots): " + std::string(buf) + "\n";
+	sprintf(buf,"%02d", (int) xBTprofile->getShipDirection());
+	tmp += " Ship Direction at Launch (Degrees):   " + std::string(buf) + "\n";
+	tmp += " Sequence Number:   "+ std::to_string(xBTprofile->getSequenceNumber()) + "\n";
+	tmp += " Transect Number:    " + std::to_string(xBTprofile->getTransectNumber()) + "\n";
+	sprintf(buf,"%4.2f", xBTprofile->getLaunchHeight());
+	tmp += " Launch Height (Meters):  " + std::string(buf) +  "\n";
+	tmp += " SOOP Line: " + xBTprofile->getSoopLine() + "\n";
+	tmp += " XBT Launcher Type: " + Launchers::getLauncherDescription(xBTprofile->getXBTLauncherType()) + "\n";
+	tmp += " XBT Recorder Serial Number: " + xBTprofile->getXBTRecorderSerialNumber() + "\n";
+    sprintf(buf,"%02d/%02d/%04d\n",
+            xBTprofile->getXBTRecorderManufacturedMonth(),
+            xBTprofile->getXBTRecorderManufacturedDay(),
+            xBTprofile->getXBTRecorderManufacturedYear());
+	tmp += " XBT Recorder Manufacture Date: " + std::string(buf);
+    sprintf(buf,"%02d/%02d/%04d\n",
+            xBTprofile->getXBTProbeManufacturedMonth(),
+            xBTprofile->getXBTProbeManufacturedDay(),
+            xBTprofile->getXBTProbeManufacturedYear());
+	tmp += " Probe Manufacture Date: " + std::string(buf);
+	tmp += " Agency in charge of Operation: " + Agency::getAgencyDescription(xBTprofile->getAgencyOwner()) + "\n";
+	tmp += " Ship Rider: " + xBTprofile->getRiderNames() + "\n";
+	tmp += " Ship Rider Institution: " + xBTprofile->getRiderInstitutions() + "\n";
+	tmp += " Ship Rider Email: " + xBTprofile->getRiderEmails() + "\n";
+	tmp += " Ship Rider Telephone Number: " + xBTprofile->getRiderPhones() + "\n";
+
+
+
+	return tmp;
 
 }

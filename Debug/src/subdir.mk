ce0dfa34dfe61bfdf9b5af1cb986dd4dbabb4304
qbancoffee <pedro.pena@noaa.gov>
2019-09-12 15:09:29 -0400

    renamed project name to xbtbinfileutils

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -68,7 +68,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++1y -I"/home/pedro/eclipse-workspace/amverseasbinfileutils/include" -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++1y -I"/home/pedro/eclipse-workspace/xbtbinfileutils/include" -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
